ef27923031f73fea22963e0ea571f32bc82180b2
maxmotsyk <21mechanic21@gmail.com>
2022-06-11 22:26:54 +0300

    commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const watch = require('gulp-watch');
+const clean = require('gulp-clean-css');
+
+function buildStyles() {
+   return gulp.src('./scss/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error',sass.logError))
+  .pipe(sourcemaps.write('./'))
+  .pipe(concat("main.css"))
+  .pipe(clean())
+  .pipe(gulp.dest('./css/'))
+};
+
+function watchScss(){
+  gulp.watch('./style_scss/**/*.scss',gulp.series('buildStyles'))
+}
+
+exports.buildStyles = buildStyles;
+exports.watchScss = watchScss;
